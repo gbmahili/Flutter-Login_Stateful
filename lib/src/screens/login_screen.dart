0b2ff3c250b2745e7a5e047390c5fb42b9433f7c
Baraka Mahili <georgesmahili@gmail.com>
2019-05-22 19:58:13 -0400

    Added form validation using the validate method on the currentState and validator on the forminputfield

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -8,6 +8,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenSate extends State<LoginScreen> {
+  // this will reference the form in our app
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,11 +17,14 @@ class _LoginScreenSate extends State<LoginScreen> {
       child: Container(
         margin: EdgeInsets.all(20),
         child: Form(
+          key: formKey, //refers to the key created up
           child: Column(
             children: <Widget>[
               emailField(),
               passwordField(),
-              SizedBox(height: 25,),
+              SizedBox(
+                height: 25,
+              ),
               submitButton(),
             ],
           ),
@@ -35,6 +40,14 @@ class _LoginScreenSate extends State<LoginScreen> {
         hintText: 'you@email.com',
       ),
       keyboardType: TextInputType.emailAddress,
+      validator: (String value) {
+        // This function will return null if valid
+        // other wise, it will return a string with errors
+        if (!value.contains('@')) {
+          return 'Please enter a valid email address';
+        }
+        return null;
+      },
     );
   }
 
@@ -44,16 +57,32 @@ class _LoginScreenSate extends State<LoginScreen> {
         labelText: 'Password',
         hintText: 'Password',
       ),
+      validator: (String value) {
+        if (value.length < 4) {
+          return 'Password must be at least 4 characters';
+        }
+      },
       // obscureText: true, // ******
     );
   }
 
   Widget submitButton() {
     return RaisedButton(
-      onPressed: () {},
       child: Text('Submit'),
       color: Colors.blue,
       textColor: Colors.white,
+      onPressed: () {
+        // Reset the entire form on which the formKey is referencing.
+        //formKey.currentState.reset();
+        final isFormValid = formKey.currentState.validate(); //this validate() method calls the validator on each formfield
+        if(isFormValid) {
+          print('Send to DB');
+        } else {
+          print('Errors in the form');
+          return;
+        }
+        
+      },
     );
   }
 }
