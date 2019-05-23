38025fddc51408d60c33159f3f67b3e346df33ae
Baraka Mahili <georgesmahili@gmail.com>
2019-05-22 20:16:14 -0400

    Create a way to retrieve data from the form

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -10,6 +10,10 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenSate extends State<LoginScreen> {
   // this will reference the form in our app
   final formKey = GlobalKey<FormState>();
+  // Instantiate the fields for email and password which will be updated when user clicks Submit
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -48,6 +52,10 @@ class _LoginScreenSate extends State<LoginScreen> {
         }
         return null;
       },
+      onSaved: (value) {
+        print(value);
+        email = value;
+      },
     );
   }
 
@@ -62,6 +70,10 @@ class _LoginScreenSate extends State<LoginScreen> {
           return 'Password must be at least 4 characters';
         }
       },
+      onSaved: (String value) {
+        print(value);
+        password = value;
+      },
       // obscureText: true, // ******
     );
   }
@@ -74,14 +86,18 @@ class _LoginScreenSate extends State<LoginScreen> {
       onPressed: () {
         // Reset the entire form on which the formKey is referencing.
         //formKey.currentState.reset();
-        final isFormValid = formKey.currentState.validate(); //this validate() method calls the validator on each formfield
+        //this validate() method calls the validator on each formfield and returns true or false
+        final isFormValid = formKey.currentState.validate(); 
         if(isFormValid) {
           print('Send to DB');
+          // Save data...refer to the form globakey and use its method to save...
+          formKey.currentState.save();
+          // Get email and password as they were updated
+          print('Email is: $email and Password is: $password');
         } else {
           print('Errors in the form');
           return;
         }
-        
       },
     );
   }
